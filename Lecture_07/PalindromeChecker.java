74b1063280f74bf7403373d7df3de2d5e1be8707
Nguyễn Quốc Vinh (VinKay) <88923527+vinkay215@users.noreply.github.com>
2024-11-22 00:05:48 +0700

    Check if a string is a palindrome

---
+++ Lecture_07/PalindromeChecker.java
@@ -0,0 +1,36 @@
+package Lecture_07;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to check: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
