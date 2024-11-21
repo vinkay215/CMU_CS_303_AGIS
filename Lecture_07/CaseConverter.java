4ba5390bcd6b53a18587b36ea6a4a7e3aa24006c
Nguyễn Quốc Vinh (VinKay) <88923527+vinkay215@users.noreply.github.com>
2024-11-22 00:02:19 +0700

    Assignment 8

---
+++ Lecture_07/CaseConverter.java
@@ -0,0 +1,29 @@
+package Lecture_07;
+import java.util.Scanner;
+
+public class CaseConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String converted = convertCase(input);
+        System.out.println("Converted string: " + converted);
+
+        scanner.close();
+    }
+
+    public static String convertCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isUpperCase(chars[i])) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            } else if (Character.isLowerCase(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }
+        return new String(chars);
+    }
+}
+
